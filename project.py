3960cbef4848498892f6a2e72edd9b67ba700d50
simo1704 <simo.melillo.17@gamil.com>
2026-03-26 15:23:16 +0100

    punto 15 - heatmap

--- project.py
+++ project.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('food-inspections.csv', sep=',')
+df = pd.read_csv(
+    'C:\\Users\\simom\\OneDrive - Università degli Studi di Milano-Bicocca\\3° anno\\Data Processing\\Chicago Food Inspections\\dataset\\food-inspections.csv', sep=',')
 print(df.head())
 
 # 1. Quali tipi di esercizi alimentari vengono ispezionati più spesso?
@@ -19,3 +22,12 @@ print(df.groupby(['Risk','Results'])['Inspection ID'].sum())
 # 4. Il tasso di fallimento è cambiato nel tempo?
 print(df[df['Results'] == 'Fail'].groupby('Inspection Date').size() / df.groupby('Inspection Date').size())
 
+# 15. Alcune violazioni sono fortemente associate a specifici tipi di struttura?
+data = df.select_dtypes(include='number').dropna(axis=1, how='all') # Seleziona solo le colonne numeriche e rimuove quelle con tutti i valori NaN
+
+f, ax = plt.subplots(figsize=(9, 6)) # Crea una figura e un asse per il grafico
+# Crea una heatmap della matrice di correlazione dei dati numerici, con annotazioni e formattazione dei numeri
+sns.heatmap(data.corr() , annot=True, fmt=".2f", linewidths=.5, ax=ax) 
+plt.tight_layout() # Regola automaticamente i parametri del layout per evitare sovrapposizioni
+plt.show() # Mostra il grafico
+f.savefig('correlation_heatmap.png') # Salva il grafico
